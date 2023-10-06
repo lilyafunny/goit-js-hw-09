85dbc239de9cfbff3fbdfe6a17a4a7b06752e94b
lilyafunny <125149545+lilyafunny@users.noreply.github.com>
2023-10-06 23:03:50 +0300

    01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,43 @@
+const refs = {
+  btStartEl: document.querySelector('button[data-start]'),
+  btStopEl: document.querySelector('button[data-stop]'),
+  bodyEl: document.querySelector('body'),
+};
 
+refs.btStartEl.addEventListener('click', handlClickStart);
+refs.btStopEl.addEventListener('click', handlClickStop);
+
+function handlClickStart() {
+  refs.btStartEl.setAttribute('disabled', '');
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+
+  timerId = setInterval(() => {
+    refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function handlClickStop() {
+  clearInterval(timerId);
+  refs.btStartEl.removeAttribute('disabled');
+}
+
+/*
+
+startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {
+    console.log(`I love async JS!  ${Math.random()}`);
+  }, 1000);
+});
+
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+  console.log(`Interval with id ${timerId} has stopped!`);
+});
+
+*/
