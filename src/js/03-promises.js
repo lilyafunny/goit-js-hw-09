62df16c706074826aa037b0eb2813c8e640e9c6d
lilyafunny <125149545+lilyafunny@users.noreply.github.com>
2023-10-07 21:15:34 +0300

    03-promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+const form = document.querySelector('form');
+const fD = document.querySelector('[name="delay"]');
+const ds = document.querySelector('[name="step"]');
+const amountEl = document.querySelector('[name="amount"]');
+
+form.addEventListener('submit', promise);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promise;
+}
+
+function promise(event) {
+  event.preventDefault();
+
+  let del = Number(fD.value);
+  let step = Number(ds.value);
+  let amount = Number(amountEl.value);
+
+  for (let i = 1; i <= amount; i++) {
+    let promDel = del + step * (i - 1);
+
+    createPromise(i, promDel)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
